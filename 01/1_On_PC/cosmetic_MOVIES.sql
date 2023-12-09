8e565ff56417bb704c2351bb11ebd4cb9ced2e09
scp-cloudacademy <147478897+scp-cloudacademy@users.noreply.github.com>
2023-12-09 13:57:18 +0900

    Add files via upload

---
+++ 01/1_On_PC/cosmetic_MOVIES.sql
@@ -0,0 +1,57 @@
+CREATE DATABASE  IF NOT EXISTS `cosmetic` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `cosmetic`;
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: 192.168.11.4    Database: cosmetic
+-- ------------------------------------------------------
+-- Server version	8.0.35
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `MOVIES`
+--
+
+DROP TABLE IF EXISTS `MOVIES`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `MOVIES` (
+  `ID` varchar(7) NOT NULL,
+  `TITLE_KO` varchar(256) NOT NULL,
+  `TITLE_EN` varchar(256) NOT NULL,
+  `GENRE` varchar(256) DEFAULT NULL,
+  `DIRECTOR` varchar(128) DEFAULT NULL,
+  `RELEASE_YEAR` varchar(4) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `MOVIES`
+--
+
+LOCK TABLES `MOVIES` WRITE;
+/*!40000 ALTER TABLE `MOVIES` DISABLE KEYS */;
+INSERT INTO `MOVIES` VALUES ('38ABE4Q','테이큰','Taken','스릴러','피에르 모렐','2008'),('47TZCJO','원티드','Wanted','액션','티무르','2008'),('AW4IAI8','강철중','Public Enemy Returns','범죄','강우석','2008'),('DYXZBUH','추격자','The Chaser','범죄','나홍진','2008'),('FNZS7SB','쿵푸팬더','Kung Fu Panda','애니메이션','여인영','2008'),('JY0H2O0','아이언맨','Iron Man','SF','존 파브로','2008'),('U6FHF7A','좋은 놈, 나쁜 놈, 이상한 놈','The Good, the Bad, and the Weird','액션','김지운','2008'),('VC2FLAF','핸콕','Hancock','액션','피터 버그','2008'),('VHQ4D3K','우리 생애 최고의 순간','Forever the Moment','드라마','임순례','2008');
+/*!40000 ALTER TABLE `MOVIES` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-12-03 18:01:57
